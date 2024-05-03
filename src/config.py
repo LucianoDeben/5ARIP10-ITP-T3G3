15d171dd485f26404157a706fdc9f4a290292090
LucidCodeAI <lutchocode@gmail.com>
2024-05-03 07:40:57 +0200

    Add wandb init

--- src/config.py
+++ src/config.py
@@ -1,9 +1,13 @@
-class Config:
-    learning_rate = 0.001
-    batch_size = 4
-    num_epochs = 10
-    val_frac = 0.2
-    seed = 42
+import wandb
 
-
-config = Config()
+wandb.init(
+    project="5ARIP10-T3G3",
+    entity="luciano-deben",
+    config={
+        "learning_rate": 0.001,
+        "batch_size": 4,
+        "num_epochs": 10,
+        "val_frac": 0.2,
+        "seed": 42,
+    },
+)
